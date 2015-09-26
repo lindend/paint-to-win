9f351ff050ea7aab9947a1a07f14ad15be33815e
Daniel Linden <d.linden88@gmail.com>
2015-09-26 22:57:40 +0200

    Update wordlist service debug run path

--- src/paintToWin/wordlistService/run.sh
+++ src/paintToWin/wordlistService/run.sh
@@ -2,6 +2,6 @@
 if make; then
 	../../../bin/wordlist -db "user=p2wuser password=devpassword host=localhost port=5432 dbname=paint2win sslmode=disable"\
 		ApiPort=8007 \
-		WordlistRoot=../../../../../paint-to-win-wordlists/ \
+		WordlistRoot=../../../../paint-to-win-wordlists/ \
 		Address=$(hostname -I)
 fi
